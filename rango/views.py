1e20887c0f292b591d0d37b713182b0cce6e4da8
ArranBrown <2323915b@student.gla.ac.uk>
2019-01-21 21:03:07 +0000

    added untrackted files

---
+++ rango/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    context_dict = {'boldmessage': "Cruncy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+    #return HttpResponse("Rango says hey there partner!, <br/> <a href='/rango/about/'>about </a>")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index </a>")
