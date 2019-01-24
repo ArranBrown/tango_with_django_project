2827cbffc3e9401bcd80015ffc140fc8103616d4
ArranBrown <2323915b@student.gla.ac.uk>
2019-01-24 20:30:45 +0000

    fix about template

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 
 def index(request):
     context_dict = {'boldmessage': "Cruncy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
+    # return HttpResponse("Rango says hey there partner!, <br/> <a href='/rango/about/'>about </a>")
 
 
 def about(request):
     context_dict = {'boldmessage': "Arran Brown"}
     return render(request, 'rango/about.html', context=context_dict)
+    # #return HttpResponse("Rango says here is the about page <br/> <a href='/rango/'>Index </a>")
