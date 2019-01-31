507553d8197b37bb175705b3292a7b6b71d9ba67
ArranBrown <2323915b@student.gla.ac.uk>
2019-01-31 21:00:23 +0000

    Chapter 7 done

--- rango/models.py
+++ rango/models.py
@@ -4,10 +4,12 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    max_length = 128
+    name = models.CharField(max_length=max_length, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
+
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
